8393b0a2071a3940e8eef3f6dc46ecb03242ae75
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-08 22:45:27 +0200

    Add src/components/Calculator.js file

---
+++ src/components/Calculator.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <h1>Hello Calculator</h1>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
