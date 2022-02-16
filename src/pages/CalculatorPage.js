a3400c3197f574aa395beabf861613637fbae3ec
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-16 02:19:10 +0200

    Add all single pages in the pages/ directory

---
+++ src/pages/CalculatorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+import Calculator from '../components/Calculator';
+
+const CalculatorPage = () => (
+  <main>
+    <Header />
+    <Calculator />
+  </main>
+);
+
+export default CalculatorPage;
